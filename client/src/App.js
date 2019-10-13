3ffe8fed78065d33385b00e5ef88d966ebed1a0b
yaqootturman <yaqoot.turman@gmail.com>
2019-10-13 10:50:38 +0300

    to test

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
-import PledgePage from './components/PledgePage'
 import CategoryPledges from './components/CategoryPledges'
+import PledgePage from './components/PledgePage'
 
 const App = () => (
   <Router>
     <React.Fragment>
       <Route exact path='/' component={LandingPage} />
       <Route path='/dashboard/action-category/:category_id' component={CategoryPledges} />
-      <Route exact path='/dashboard/action-category/:action_name' component={PledgePage} />
+      <Route path='/dashboard/action-category/pledge/:action_name' component={PledgePage} />
     </React.Fragment>
   </Router>
 )
--- client/src/components/PledgePage/index.js
+++ client/src/components/PledgePage/index.js
@@ -2,7 +2,11 @@ import React from "react"
 
 class PledgePage extends React.Component {
   render() {
-
+    const { data } = this.props.location.state
+    return (<div>
+      <h1>helllo</h1>
+      {console.log("this.props.location.data", data)}
+    </div>)
   }
 }
 export default PledgePage

6e243b2c7f6f401245715e48175119a011b12cfb
yaqootturman <yaqoot.turman@gmail.com>
2019-10-15 16:17:07 +0300

    show data of pledge

--- client/src/components/PledgePage/index.js
+++ client/src/components/PledgePage/index.js
@@ -1,26 +1,92 @@
 import React from "react"
 import axios from 'axios'
 class PledgePage extends React.Component {
+  state = {
+    pledgeInfo: [],
+    pledgeInstructions: [],
+    pledgeProsCons: [],
+    pledgeResources: [],
+    pledgeReferences: []
+  }
 
   componentDidMount() {
     const { pledge_id } = this.props.location.state
+
+
     axios.get(`/api/action-category/pledge/${pledge_id}`).then(Response => {
-      const pledge_info_page = Response.data
+      const pledgeInfoPage = Response.data
+      console.log("Response", pledgeInfoPage[0]);
+      this.setState({
+        pledgeInfo: pledgeInfoPage[0],
+        pledgeInstructions: pledgeInfoPage[1],
+        pledgeProsCons: pledgeInfoPage[2],
+        pledgeResources: pledgeInfoPage[3],
+        pledgeReferences: pledgeInfoPage[4]
+      })
 
-      console.log("Response", pledge_info_page);
-
-      //here should handle what we recieve from database which should change to rexieve sth i can read 
     }).catch(err => { console.log("0000", err) });
 
   }
 
   render() {
     const { data } = this.props.location.state
+    const { pledgeInfo, pledgeInstructions, pledgeProsCons, pledgeResources, pledgeReferences } = this.state
+    {/* should axios request to get pledge info by id */ }
+    {/* {console.log("this.props.location.data", data)} */ }
+    return (
+      <>
+        {
+          !pledgeInfo.length ? <p>Loading...</p> :
+            <>
+              <p>{pledgeInfo[0].title}</p>
+              <p>{pledgeInfo[0].description}</p>
+              <p>{pledgeInfo[0].importance}</p>
+              <p>{pledgeInfo[0].img}</p>
+              <p>{pledgeInfo[0].number_of_enrollement}</p>
+              <p>{pledgeInfo[0].further_info}</p>
+              {console.log("whhhat", pledgeInstructions)
+              }
+              {pledgeInstructions.map((element, index) => {
+                return (
+                  <>
+                    <p>{element.description}</p>
+                  </>
+                )
+              })}
+              {pledgeProsCons.map((element, index) => {
+                return (
+                  <>
+                    <p>{element.statement}</p>
+                    <p>{element.color}</p>
 
-    return (<div>
-      {/* should axios request to get pledge info by id */}
-      {/* {console.log("this.props.location.data", data)} */}
-    </div>)
+                  </>
+                )
+              })}
+
+              {pledgeResources.map((element, index) => {
+                return (
+                  <>
+                    <p>{element.description}</p>
+
+                  </>
+                )
+              })}
+
+
+              {pledgeReferences.map((element, index) => {
+                return (
+                  <>
+                    <p>{element.description}</p>
+                  </>
+                )
+              })}
+            </>
+
+
+        }
+
+      </>
+    )
   }
 }
 export default PledgePage

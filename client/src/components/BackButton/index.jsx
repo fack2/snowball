516b62a2bab8d3f07c2ed5bfd9f8b53fa909af5c
marcy corps <ishak-3@hotmail.com>
2019-10-15 15:46:43 +0300

    test

--- client/src/components/BackButton/index.jsx
+++ client/src/components/BackButton/index.jsx
@@ -11,6 +11,9 @@ class BackButton extends Component {
         <button className="Back__button" onClick={ ()=>{
           this.goBack();
         } } > <div className="Back__button-inner"><img src="https://i.ibb.co/47c20FN/Polygon.png" alt="arrow-back" /><p>BACK</p></div></button>
+        <button className="Back__button" onClick={ ()=>{
+          this.goBack();
+        } } > <div className="Back__button-inner"><img src="https://i.ibb.co/47c20FN/Polygon.png" alt="arrow-back" /><p>BACK</p></div></button>
       </div>
     )
   }

69a2c92bee124fffb7420aaacfb335de7fcdb5b8
yaqootturman <yaqoot.turman@gmail.com>
2019-10-16 09:02:58 +0300

    start styling

--- client/src/components/PledgePage/index.js
+++ client/src/components/PledgePage/index.js
@@ -12,7 +12,6 @@ class PledgePage extends React.Component {
   componentDidMount() {
     const { pledge_id } = this.props.location.state
 
-
     axios.get(`/api/action-category/pledge/${pledge_id}`).then(Response => {
       const pledgeInfoPage = Response.data
       this.setState({
@@ -36,46 +35,62 @@ class PledgePage extends React.Component {
         {
           !pledgeInfo.length ? <p>Loading...</p> :
             <>
-              <p>{pledgeInfo[0].title}</p>
-              <p>{pledgeInfo[0].description}</p>
-              <p>{pledgeInfo[0].importance}</p>
-              <p>{pledgeInfo[0].img}</p>
-              <p>{pledgeInfo[0].number_of_enrollement}</p>
-              <p>{pledgeInfo[0].further_info}</p>
+              <div className="top-info">
+                <img className="top-info__img" src={pledgeInfo[0].img} />
+                <p className="top-info__title">{pledgeInfo[0].title}</p>
+                <p className="top-info__description">{pledgeInfo[0].description}</p>
+                <p className="top-info__number">{pledgeInfo[0].number_of_enrollement}</p>
+              </div>
+
+              <div className="importance-div">
+                <h3>Why it`s important?</h3>
+                <p>{pledgeInfo[0].importance}</p>
+              </div>
+
+              <h3>How to do it</h3>
               {pledgeInstructions.map((element, index) => {
                 return (
-                  <>
+                  <div>
                     <p>{element.description}</p>
-                  </>
+                  </div>
                 )
               })}
+
+              <h3>Pros & cons of pledge</h3>
               {pledgeProsCons.map((element, index) => {
                 return (
-                  <>
+                  <div>
                     <p>{element.statement}</p>
                     <p>{element.color}</p>
 
-                  </>
+                  </div>
                 )
               })}
 
+              <h3>Resources to help</h3>
               {pledgeResources.map((element, index) => {
                 return (
-                  <>
+                  <div>
                     <p>{element.description}</p>
 
-                  </>
+                  </div>
                 )
               })}
 
+              <div>
+                <h3>Further information</h3>
+                <p>{pledgeInfo[0].further_info}</p>
+              </div>
 
+              <h3>References</h3>
               {pledgeReferences.map((element, index) => {
                 return (
-                  <>
+                  <div>
                     <p>{element.description}</p>
-                  </>
+                  </div>
                 )
               })}
+
             </>
 
 
